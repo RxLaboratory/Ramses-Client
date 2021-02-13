3203f41a9176b46329ef604f4f4414f5521c2ed7
Duduf <git@duduf.com>
2021-02-13 19:21:14 +0100

    login on enterpressed

--- src/loginpage.cpp
+++ src/loginpage.cpp
@@ -15,6 +15,8 @@ LoginPage::LoginPage(QWidget *parent) :
 
     connect(_ramses,&Ramses::loggedIn, this, &LoginPage::loggedIn);
     connect(_ramses,&Ramses::loggedOut, this, &LoginPage::loggedOut);
+    connect(usernameEdit, &QLineEdit::returnPressed, this, &LoginPage::loginButton_clicked);
+    connect(passwordEdit, &QLineEdit::returnPressed, this, &LoginPage::loginButton_clicked);
     connect(DBInterface::instance(), &DBInterface::log, this, &LoginPage::dbiLog);
     connect(serverSettingsButton, SIGNAL(clicked()), this, SLOT(serverSettingsButton_clicked()));
     connect(loginButton, SIGNAL(clicked()), this, SLOT(loginButton_clicked()));
