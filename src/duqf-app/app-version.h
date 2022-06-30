b1edd4d0ddcd69188e835c2801225a1b1eb0eff7
Duduf <git@duduf.com>
2022-06-30 13:50:14 +0200

    changed version to 0.3.0

--- src/duqf-app/app-version.h
+++ src/duqf-app/app-version.h
@@ -2,8 +2,8 @@
 #define APPVERSION_H
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 2
-#define VERSION_BUILD 14
+#define VERSION_MINOR 3
+#define VERSION_BUILD 0
 #define VERSION_SUFFIX "Alpha"
 
 #define STRINGIFY_VERSION(A, B, C) CONCAT(A, B, C )
