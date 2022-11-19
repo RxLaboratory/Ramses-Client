c2e16e890b1351af302a0b0c9df80a5053dc998f
Duduf <git@duduf.com>
2022-11-19 11:59:56 +0100

    doc & comments

--- src/ramobjectmodels/dbtablemodel.h
+++ src/ramobjectmodels/dbtablemodel.h
@@ -13,26 +13,36 @@ public:
     DBTableModel(RamAbstractObject::ObjectType type, QObject *parent = nullptr);
 
     // Parameters to be set before load()
+    /**
+     * @brief setFilterKey Sets the key to be used in the data to filter the objects loaded in this table
+     * @param key The key, as used in the JSON data
+     */
     void setFilterKey(QString key);
+    /**
+     * @brief addFilterValue Adds a value to be accepted with the filter key
+     * @param value The value
+     */
     void addFilterValue(QString value);
 
-    // Call it (at least) once to do the initial loading.
-    // Does not reload if it's already been loaded, call reload() instead.
-    // Calling it the first time the model is needed, as late as possible, may improve performance.
+    /**
+     * @brief load Initial loading of the table
+     * Call it (at least) once to do the initial loading.
+     * Calling it the first time the model is actually needed, as late as possible, may improve performance.
+     */
     void load();
 
 private slots:
+    // Inserts a new object
     void insertObject(QString uuid, QString table);
+    // Removes an object
     void removeObject(QString uuid, QString table);
+    // Clear and reload the data
     void reload();
+    // Save the order of the objects when the rows have been moved
     void saveOrder(const QModelIndex &parent, int start, int end, const QModelIndex &destination, int row);
 
 private:
-    // Initial loading
-    // Change: must be explicitly called
-    // to allow setting filters before
-    // It is now the public method load()
-    // void construct();
+    // This flag is used to check if the table has already been loaded.
     bool m_isLoaded =false;
 
     // Parameters
